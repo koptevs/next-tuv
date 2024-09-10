0bbe8d677c41e5370db58524b258baeb2d666dad
Igors Koptevs <igors.koptevs@gmail.com>
2024-09-10 20:15:36 +0300

    Added custom button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ButtonShevronLucide from "@/components/button-shevron-lucide";
 import { ModeToggle } from "@/components/mode-toggler";
 import Image from "next/image";
 
@@ -6,6 +7,7 @@ export default function Home() {
         <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
             <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
                 <ModeToggle />
+                <ButtonShevronLucide>Shevron</ButtonShevronLucide>
                 <Image
                     className="dark:invert"
                     src="https://nextjs.org/icons/next.svg"
