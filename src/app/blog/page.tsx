ca5523ba94d04a44fbd558aca7badfdf4a33ab21
Igors Koptevs <igors.koptevs@gmail.com>
2024-09-23 08:39:13 +0300

    Changed to async getPublicDomain function

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,7 @@ import PostCard from "./card";
 // no-store
 
 async function getData() {
-    const domain = getPublicDomain();
+    const domain = await getPublicDomain();
     const endpoint = `${domain}/api/posts`;
     // const response = await fetch(endpoint, {
     //     next: {
