c12b65105bad6c42b47bc046e0779d3e3f07fb5a
Igors Koptevs <igors.koptevs@gmail.com>
2024-09-23 13:41:47 +0300

    Client side getching Github

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,10 +1,21 @@
 "use client";
 
-import React from "react";
-// import useSWR from "swr";
+import useSWR from "swr";
 
-// const fetcher = (url) => fetch(url).then((res) => res.json());
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function GithubPage() {
-    return <div>GithubPage</div>;
+    const myGithubRepo = "https://api.github.com/repos/koptevs/next-tuv";
+    const { data, error, isLoading } = useSWR(myGithubRepo, fetcher);
+    if (error) {
+        return <div className="container">Error occured</div>;
+    }
+    if (isLoading) {
+        return <div className="container">Loading...</div>;
+    }
+    return (
+        <div className="container">
+            <h3>{data.name}</h3>
+        </div>
+    );
 }
